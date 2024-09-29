31df42bdeaf79fc38933a02a3b2562bf2d1d9bab
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2024-09-29 19:44:38 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -8,15 +8,15 @@ from model import UNet2DModel
 
 def main(
         checkpoint: str,
-        num_samples: int = 64,
+        num_samples: int = 4,
         num_inference_steps: int = 35,
         device: str = 'cuda:0',
         save_dir: str = 'samples',
         tile_size: int = 4,
         seed: int = 24357234501,
         eta: float = 0.0,
-        class_labels: int = None,
-        unconditional: bool = False,
+        class_labels: int = 1,
+        unconditional: bool = True,
 ):
     print(f'checkpoint {checkpoint}')
     print(f'num samples {num_samples}')
@@ -32,22 +32,15 @@ def main(
     pipeline = KarrasPipeline.from_pretrained(checkpoint)
     pipeline.to(device)
 
-    if class_labels and not unconditional:
-        print(f'class labels {class_labels}')
-        class_labels = torch.randint(0, class_labels, (num_samples,))
-    elif class_labels and unconditional:
-        class_labels = -torch.ones((num_samples,), dtype=torch.long)
-    else:
-        assert pipeline.unet.num_class_embeds is None, 'Model has class embeddings, but no class labels were provided. For conditional sampling set class_labels > 0. For unconditional sampling, in this case set class_labels > 0 and unconditional=True'
     images = pipeline(
-        batch_size=num_samples,
+        batch_size=4,
         num_inference_steps=num_inference_steps,
         return_dict=True,
         output_type='tensor',
         to_device='cpu',
         generator=torch.manual_seed(seed),
         eta=eta,
-        class_labels=class_labels,
+        class_labels=None,
     ).images
 
     os.makedirs(f'{save_dir}', exist_ok=True)
